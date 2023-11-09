d4cc6ff7910f0a67189c623a2d7fe931c6cbb657
paulinalh <126215454+carcienegam@users.noreply.github.com>
2023-11-09 11:27:31 -0600

    Initial Commit

---
+++ FemCoding/FemCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FemCodingApp.swift
+//  FemCoding
+//
+//  Created by Alumno on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FemCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
