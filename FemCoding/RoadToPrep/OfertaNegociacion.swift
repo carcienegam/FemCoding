5410d6dcc88fc3bbc18e8c04c774bcfc634bdef8
Val <93287062+Valenriquez@users.noreply.github.com>
2023-11-11 21:47:34 -0800

    Add files via upload

--- FemCoding/RoadToPrep/OfertaNegociacion.swift
+++ FemCoding/RoadToPrep/OfertaNegociacion.swift
@@ -1,15 +1,66 @@
-//
-//  OfertaNegociacion.swift
-//  FemCoding
-//
-//  Created by Alumno on 09/11/23.
-//
-
 import SwiftUI
 
 struct OfertaNegociacion: View {
+    @State private var currentField = ""
+    @State private var currentSalary = ""
+    @State private var currentPosition = ""
+    
+    @State private var desiredSalary = ""
+    @State private var desiredLocation = ""
+    @State private var desiredHours = ""
+    
+    @State private var negotiationTips: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            Section("Oferta Actual") {
+                TextField("Carrera STEM", text: $currentField)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                TextField("Salario Actual", text: $currentSalary)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                
+                TextField("Puesto Actual", text: $currentPosition)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            Section("Oferta Deseada") {
+                TextField("Salario Deseado", text: $desiredSalary)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                
+                TextField("Ubicación Deseada", text: $desiredLocation)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                TextField("Horas a la Semana Deseadas", text: $desiredHours)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+            }
+            
+            Spacer() // Add Spacer to push the content to the top
+            
+            Button("Obtener Consejos de Negociación") {
+                generateNegotiationTips()
+            }
+            
+            Text(negotiationTips)
+                .multilineTextAlignment(.leading)
+        }
+        .padding()
+    }
+    
+    private func generateNegotiationTips() {
+        // Logic to generate negotiation tips based on the desired offer
+        // (similar to the previous implementation)
+        negotiationTips = """
+        Consejos para negociar tu nueva oferta:
+        - Investiga los salarios promedio en tu campo y ubicación.
+        - Destaca tus logros y contribuciones específicas en proyectos relevantes.
+        - Considera beneficios no salariales que puedan ser importantes para ti.
+        - Prepárate para hablar sobre tu desarrollo profesional y cómo has crecido en tu rol.
+        - Mantén una actitud positiva y enfócate en el valor que aportas a la empresa.
+        """
     }
 }
 
@@ -18,3 +69,4 @@ struct OfertaNegociacion_Previews: PreviewProvider {
         OfertaNegociacion()
     }
 }
+
